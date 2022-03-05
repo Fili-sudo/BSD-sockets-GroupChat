6ed8b9fd43791fa73150614245ba28334d9f62ca
Fili-sudo <79535488+Fili-sudo@users.noreply.github.com>
2022-03-05 15:17:53 +0200

    Add files via upload
    Wrote server and communication functions

---
+++ serv_com.c
@@ -0,0 +1,96 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+struct client_info {
+	int sockno;
+	char ip[INET_ADDRSTRLEN]; //INET_ADDRSTRLEN spatiu de memorie pentru o adresa IPv4
+};
+
+int clients[100];
+int no_clients = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //blocam datele atunci cand un thread acceseaza o variabila comuna
+
+//trimitem mesaje la toti clientii serverului
+void sendToAll(char* msg, int curr_client)
+{
+	int i;
+	pthread_mutex_lock(&mutex);
+	
+	for (i=0;i<no_clients;i++) 
+	{
+		if (clients[i]!=curr_client)/* trimitem mesaje tuturor clientilor mai putin celui ce 
+                                         a trimis mesaj serverului pentru a evita afisarea mesajelor duplicate*/
+		{
+			if (send(clients[i],msg,strlen(msg),0) < 0) 
+			{
+				perror("sending failure...");
+				continue;
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&mutex);
+}
+
+// se primeste mesaj de la client.
+void* recvMsg(void* socket)
+{
+	struct client_info cl = *((struct client_info*)socket);
+	char buff[500],user[32];
+    char *p,*msg;
+	int msg_len,i,j;
+
+	
+    user[0]='\0';
+	while ((msg_len = recv(cl.sockno, buff, 500, 0)) > 0) 
+	{
+        buff[msg_len] = '\0';
+        /* alocam dinamic mesajul primit de la un client ca sa il redirectionam
+           catre toti ceilalti clienti conectati la chat*/
+        if((msg=(char *)malloc(msg_len*sizeof(char)))==NULL){ 
+            perror("allocation error");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(msg,buff);
+
+        if(strlen(user) == 0)
+        {
+            p=strtok(buff,":");
+            strcpy(user,p);
+        }
+
+		sendToAll(msg, cl.sockno);
+        free(msg);
+	}
+	
+    
+    /* odata ajunsi aici stim ca un client s-a deconectat. Scoatem clientul din vectorul de clienti
+       si notificam restul clientilor */
+    sprintf(buff, "%s left the chat\n", user);
+    sendToAll(buff, cl.sockno);
+
+	pthread_mutex_lock(&mutex);
+
+	printf("%s disconnected...\n", cl.ip);
+   
+	for (i=0;i<no_clients;i++) 
+	{
+		if (clients[i] == cl.sockno) 
+		{ 
+            for (j=i;j<no_clients-1;j++)
+			{
+				clients[j]=clients[j+1];
+			}
+		}
+	}
+
+	no_clients--;
+	pthread_mutex_unlock(&mutex);
+}
---
+++ server.c
@@ -0,0 +1,79 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include "/home/davidvid/ourChat/serv_com.c"
+
+
+int main(int argc, char* argv[])
+{
+	struct sockaddr_in server_addr, client_addr;
+	struct client_info cl;
+	socklen_t client_addr_size;
+	pthread_t snd_th, rcv_th;
+	int server_socket,client_socket,portno,len;
+	char msg[500];
+	char ip[INET_ADDRSTRLEN];
+
+	if (argc > 2) 
+	{
+		perror("too many arguments...");
+		exit(1);
+	}
+	
+	portno = atoi(argv[1]);
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);//cream socket pentru adresa IPv4 pe protocol TCP/IP											
+	if (server_socket < 0)																				
+	{
+		perror("opening channel unsuccessful...");
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;																	
+	server_addr.sin_port = htons(portno);																
+	server_addr.sin_addr.s_addr = inet_addr("192.168.0.106");												
+	client_addr_size = sizeof(client_addr);
+
+	if (bind(server_socket, (struct sockaddr*) & server_addr, sizeof(server_addr)) != 0)
+	{
+		perror("binding unsuccessful...");
+		exit(1);
+	}
+
+	if (listen(server_socket, 5) != 0)																
+	{
+		perror("listening unsuccessful...");
+		exit(1);
+	}
+
+	while (1) 
+	{
+		if ((client_socket = accept(server_socket, (struct sockaddr*) & client_addr, &client_addr_size)) < 0)	// Accepts a connection request - blocking call.
+		{
+			perror("accept unsuccessful...");
+			exit(1);
+		}
+	
+		pthread_mutex_lock(&mutex);
+		inet_ntop(AF_INET, (struct sockaddr*) & client_addr, ip, INET_ADDRSTRLEN);
+		
+		printf("%s connected...\n", ip); //adaugam noul client in vectorul de clienti
+
+		cl.sockno = client_socket; 
+		strcpy(cl.ip, ip);
+		clients[no_clients] = client_socket;
+		no_clients++;
+		
+		pthread_create(&rcv_th, NULL, recvMsg, &cl); //cream un thread care sa se ocupe de respectivul client
+		pthread_mutex_unlock(&mutex);
+	}
+
+    close(server_socket);
+	return 0;
+}
+
